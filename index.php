28c78533e2deb3a3bf2d6389c29ec141ea0c65a6
SiProdZz <j.bros@hotmail.fr>
2020-04-09 17:52:31 +0200

    create .htaccess folder, and 404 error emtpy files.

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
 require "controller/frontend.php";
+
+switch ($_GET['action']) {
+
+	case 'home':
+		echo "page d'accueil";
+		break;
+
+	case 'listeBlog':
+		echo "Liste des blogs";
+		break;
+
+
+
+	default:
+		echo "page d'accueil";
+		break;
+}
