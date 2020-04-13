b15f9bcf7ceaf686ec5f6ad7badb2db48a05ddae
SiProdZz <j.bros@hotmail.fr>
2020-04-13 16:19:58 +0200

    connexion database

--- model/Manager.php
+++ model/Manager.php
@@ -1,1 +1,11 @@
 <?php
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
