4344c6eff13beb476fb0838dabc2d0d849af2aa4
SiProdZz <j.bros@hotmail.fr>
2020-06-19 11:01:13 +0200

    build class project class controller class services (mail check image) and still try to respect psr1 & 2

---
+++ src/Services/VerificationFichierImage.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Services;
+
+class VerificationFichierImage{
+    
+    public function verificationFichierImage(){
+        
+        if (!isset($_FILES["image"]) ||  $_FILES['image']['name'] == "") { 
+            $GLOBALS['$messageServeur'] = '<p id="messageServeur">Veuillez insérez une image.</p>';
+        } else {
+            $move = "/../../public/images/blogs/";
+            $nom_fichier = $_FILES['image']['name'];
+            $source = $_FILES['image']['tmp_name'];
+            $type_fichier = $_FILES['image']['type'];
+            $taille_fichier = $_FILES['image']['size'];
+            if ($nom_fichier == ".htaccess") {
+                $GLOBALS['$messageServeur'] = '<p id="messageServeur">Le fichier .htaccess n\'est pas autorisé.</p>';
+            } elseif ($type_fichier != "image/jpeg" && $type_fichier != "image/jpg" && $type_fichier != "image/png") {
+                $GLOBALS['$messageServeur'] = '<p id="messageServeur">L\'image doit être de type jpeg, jpg ou png.</p>';
+            } elseif ($taille_fichier >= 2097153) {
+                $GLOBALS['$messageServeur'] = '<p id="messageServeur">L\'image ne doit pas dépassé 2GO</p>';
+            } else {
+                move_uploaded_file($_FILES['image']['tmp_name'], __DIR__.$move. $_FILES["image"]['name']);
+                $tabRemplacement = array("é"=>"e", "è"=>"e", "à"=>"a"); //Changing special characters
+                $handle = fopen("public/images/blogs/".$nom_fichier, 'r'); //ouverture du fichier
+                $content = fread($handle, $taille_fichier); //lecture du fichier
+                $encoded_content = chunk_split(base64_encode($content)); //Encoding
+                $f = fclose($handle); //File closing
+                $GLOBALS['$imageValider'] = true;
+            }
+        }
+    }
+
+}
